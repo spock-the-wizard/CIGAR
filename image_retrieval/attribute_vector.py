7eeda90b4cf134eb4158d8406cae63f8e825a667
deokhk <deokhk@postech.ac.kr>
2022-04-12 03:43:55 +0900

    category to one-hot intermediate commit

---
+++ image_retrieval/attribute_vector.py
@@ -0,0 +1,133 @@
+import numpy as np
+import json
+import argparse
+import itertools
+import os
+import pickle
+import sys
+
+sys.path.append("/home/deokhk/coursework/CIGAR/")
+from tqdm import tqdm
+from utils.util import extract_subset
+
+"""
+This file saves pickle file with type dict(numpy).
+Key in dict corresponds to imageid and value is a attribute vector.
+Here, we generate three types of attribute vectors.
+[1] Sum of one-hot vector [1000d] 
+[2] Bert embedding given a list of attributes [512d]
+[3] Sum of word vectors [256d]
+"""
+
+def convert_to_one_hot(dress_to_attr, shirt_to_attr, toptee_to_attr, attr2idx, deepfashion_img_category_path, deepfashion_img_attr_path):
+    dress_to_vector = dict()
+    shirt_to_vector = dict()
+    toptee_to_vector = dict()
+    lower_body_to_vector = dict()
+
+    for k, attr_list in tqdm(dress_to_attr.items()):
+        zero_v = np.zeros(1000) # We have 1000 attributes
+        for attr in attr_list:
+            idx = attr2idx[attr]
+            zero_v[idx] = 1
+        dress_to_vector[k] = zero_v
+    
+    for k, attr_list in tqdm(shirt_to_attr.items()):
+        zero_v = np.zeros(1000) # We have 1000 attributes
+        for attr in attr_list:
+            idx = attr2idx[attr]
+            zero_v[idx] = 1
+        shirt_to_vector[k] = zero_v
+
+    for k, attr_list in tqdm(toptee_to_attr.items()):
+        zero_v = np.zeros(1000) # We have 1000 attributes
+        for attr in attr_list:
+            idx = attr2idx[attr]
+            zero_v[idx] = 1
+        toptee_to_vector[k] = zero_v
+
+    subset = extract_subset(deepfashion_img_category_path, 
+                            deepfashion_img_attr_path)
+    
+    for elem in subset:
+        file_name, attr_list = elem[0], elem[1]
+        zero_v = np.zeros(1000) # We have 1000 attributes
+        for idx, attr_v in enumerate(attr_list):
+            if attr_v == 1:
+                zero_v[idx] = 1
+        lower_body_to_vector[file_name] = zero_v
+
+    return dress_to_vector, shirt_to_vector, toptee_to_vector, lower_body_to_vector
+    
+
+
+def convert_to_bert(dress_to_attr, shirt_to_attr, toptee_to_attr):
+    raise NotImplementedError
+
+def convert_to_wordvector(dress_to_attr, shirt_to_attr, toptee_to_attr):
+    raise NotImplementedError
+
+def save_to_vector_to_pickle(dress_vectors, shirt_vectors, toptee_vectors, lower_body_to_vector, mode, data_path="/home/deokhk/coursework/CIGAR/data/image_retrieval/"):
+    dress_filename = f"dress_{mode}.pickle"
+    shirt_filename = f"shirt_{mode}.pickle"
+    toptee_filename = f"toptee_{mode}.pickle"
+    lower_filename = f"lower_{mode}.pickle"
+
+    dress_path = os.path.join(data_path, dress_filename)
+    shirt_path = os.path.join(data_path, shirt_filename)
+    toptee_path = os.path.join(data_path, toptee_filename)
+    lower_path = os.path.join(data_path, lower_filename)
+
+    path_list = [dress_path, shirt_path, toptee_path, lower_path]
+    vectors = [dress_vectors, shirt_vectors, toptee_vectors, lower_body_to_vector]
+    for path, vector in zip(path_list, vectors):
+        with open(path, 'wb') as f:
+            pickle.dump(vector, f)
+
+def main(args):
+    with open(args.attribute2_idx_path, 'r') as f:
+        attr2idx = json.load(f)
+    dress_to_attr = dict()
+    shirt_to_attr = dict()
+    toptee_to_attr = dict()
+    for category in ["dress","shirt", "toptee"]:
+        for split in ["train", "val"]:
+            file_name = f"asin2attr.{category}.{split}.json"
+            file_path = os.path.join(args.fashioniq_path, file_name)
+            with open(file_path, 'r') as f:
+                fashion = json.load(f)            
+            for k,v in fashion.items():
+                attr_list = list(itertools.chain(*v))
+                if category == "dress":
+                    dress_to_attr[k] = attr_list
+                elif category == "shirt":
+                    shirt_to_attr[k] = attr_list
+                else:
+                    toptee_to_attr[k] = attr_list
+    
+    print(f"=== Converting fashioniq attributes to {args.mode} embeddings === ")
+    if args.mode == "one-hot":
+        dress_vectors, shirt_vectors, toptee_vectors, lower_body_to_vector = convert_to_one_hot(dress_to_attr, shirt_to_attr, toptee_to_attr, attr2idx,
+        "/home/deokhk/coursework/Category_and_Attribute/Anno_coarse/list_category_img.txt", "/home/deokhk/coursework/Category_and_Attribute/Anno_coarse/list_attr_img.txt")
+        save_to_vector_to_pickle(dress_vectors, shirt_vectors, toptee_vectors, args.mode, lower_body_to_vector)
+
+    elif args.mode == "bert":
+        pass
+        #dress_vectors, shirt_vectors, toptee_vectors = convert_to_bert(dress_to_attr, shirt_to_attr, toptee_to_attr)
+    elif args.mode == "word-vector":
+        pass
+        #dress_vectors, shirt_vectors, toptee_vectors = convert_to_wordvector(dress_to_attr, shirt_to_attr, toptee_to_attr)
+    else:
+        raise ValueError("Invalid attribute vector type!")
+
+    # Save converted fashionIQ vectors
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--fashioniq_path", type=str, help="Path to the fashioniq dataset", default="/home/deokhk/coursework/fashion-iq/data")
+    parser.add_argument("--deepfashion_path", type=str, help="Path to the deepfashion dataset", default="")
+    parser.add_argument("--attribute2_idx_path", type=str, help="Path to attribute2idx.json file", default="/home/deokhk/coursework/Category_and_Attribute/Anno_coarse/attribute2idx.json")
+    parser.add_argument("--mode", choices=["one-hot", "bert", "word-vector"], default="one-hot")
+    args = parser.parse_args()
+    main(args)
--- utils/util.py
+++ utils/util.py
@@ -1,4 +1,6 @@
 import os
+import json
+from tqdm import tqdm
 
 def name_to_filepath(dir,name):
     fnames = []
@@ -7,4 +9,74 @@ def name_to_filepath(dir,name):
             fnames.append(os.path.join(dir,fname))
 
     return fnames
+
+def create_attribute2idx(attribute_file_path="/home/deokhk/coursework/Category_and_Attribute/Anno_coarse/list_attr_cloth.txt"):
+    """
+    attribute_file_path : deepfashion attribute_list.txt
+    """
+    with open(attribute_file_path, 'r') as f:
+        lines = f.readlines()
+    header = lines[0]
+    num_attr = lines[1]
+    attribute2idx = dict()
+    for idx, line in enumerate(lines[2:]):
+        attr = line[:-2].strip()
+        attr_end = attr[-1]
+        if attr_end.endswith("1") or attr_end.endswith("2") or attr_end.endswith("3") or attr_end.endswith("4") or attr_end.endswith("5"):
+            attr = attr[:-1].strip()
+        attribute2idx[attr] = idx
     
+    path = os.path.join(os.path.dirname(attribute_file_path), 'attribute2idx.json')
+    with open(path, 'w') as json_file:
+        json.dump(attribute2idx, json_file)
+
+def extr_data_txt(path):
+    """
+    Load data from text file.
+    each entry is a dictionary whose key is a imagepath and have a corresponding value.
+    """
+    print("Reading data txt..")
+    with open(path, "r") as f:
+        data = []
+        for itr, line in tqdm(enumerate(f)):
+            # Because we got annotation in the first two lines
+            if itr >= 2:
+                image_path, value = line.split()
+                data.append([image_path, value])
+    return data
+
+def extr_data_txt_attr_img(path):
+    """
+    Load data from text file.
+    suitable for list_attr_img.txt
+    """
+    print("Reading attr txt..")
+    with open(path, "r") as f:
+        data = []
+        for itr, line in tqdm(enumerate(f)):
+            # Because we got annotation in the first two lines
+            if itr >= 2:
+                lines = line.split()
+                image_path = lines[0]
+                attrs = [int(v) for v in lines[1:]]
+                data.append([image_path, attrs])
+    return data
+
+def extract_subset(list_category_image_path, list_attr_image_path):
+    category_lower_body_range = range(21, 37)
+    data = extr_data_txt(list_category_image_path)
+    subset = set()
+    print("Now generating subset..")
+    for e in tqdm(data):
+        if int(e[1]) in category_lower_body_range:
+            subset.add(e[0])
+
+    print("Now making imagepath-attribute pair belongs to the subset")
+    data = extr_data_txt_attr_img(list_attr_image_path)
+    extracted_lower_attrs = []
+    for e in tqdm(data):
+        if e[0] in subset:
+            extracted_lower_attrs.append(e)
+    
+    return extracted_lower_attrs
+
