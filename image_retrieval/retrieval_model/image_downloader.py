bdf8f5e7ea1af56b02a0410a23f02197412069b3
Ubuntu <ubuntu@ip-172-31-31-162.ec2.internal>
2022-04-25 01:56:47 +0000

    resolve dependency  issue in aws instance

--- image_retrieval/retrieval_model/image_downloader.py
+++ image_retrieval/retrieval_model/image_downloader.py
@@ -16,12 +16,13 @@ def run(line):
     image_id, image_url = line.strip().split('\t')
     image_id, image_url = image_id.strip(), image_url.strip()
 
-    dst_path = os.path.join(f'data/images/{image_id}.jpg')
+    dst_path = os.path.join('data/images/{}.jpg'.format(image_id))
+#    if not os.path.exists()
     if not os.path.exists(dst_path):
-        print(f"{dst_path}")
+        print(dst_path)
         if image_id in broken_links:
             shutil.copy(
-                os.path.join(f'data/metadata/image_url/broken_links/{image_id}.jpg'),
+                os.path.join('data/metadata/image_url/broken_links/{}.jpg',format(image_id)),
                 dst_path
             )
         else:
@@ -31,7 +32,7 @@ def run(line):
                     dst_path
                 )
             except Exception as err:
-                print(f"[{err}] {image_id}: {image_url}")
+                print("[{}] {}: {}".format(err, image_id, image_url))
 
 
 if __name__ == '__main__':
@@ -39,10 +40,10 @@ if __name__ == '__main__':
     pool = Pool(4)
     lines = []
     for target in ['dress', 'shirt', 'toptee']:
-        with open(f'data/metadata/image_url/asin2url.{target}.txt', 'r') as f:
+        with open('data/metadata/image_url/asin2url.{}.txt'.format(target), 'r') as f:
             lines.extend(f.readlines())
 
-    print(f"Total number of images: {len(lines)}")
+    print("Total number of images: {}".format(len(lines)))
     pool.map(run, lines)
 
     print("Fashion IQ dataset has been downloaded.")
